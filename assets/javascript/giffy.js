c6013c4e8480cc25b0b4674ea5a44313d9516b25
Christopher Zenner <christopher.zenner@gmail.com>
2020-03-21 22:06:08 -0700

    feat: Display response images
    - Access response image sources and titles and then display response
      results in UI (`giffy.js`).

--- assets/javascript/giffy.js
+++ assets/javascript/giffy.js
@@ -14,15 +14,27 @@ $(document).ready(function() {
         queryURL = "https://api.giphy.com/v1/gifs/search?api_key=X6f0Yx1wXCdYoPDqRVxg1BSA17QzInZZ&q=" + animal + "&limit=10&rating";
 
     // DEBUG:
-    console.log(queryURL);
+    // console.log(queryURL);
 
     $.ajax({
       url: queryURL,
       method: "GET"
     })
     .then (function(res) {
+      // DEBUG:
       console.log(res);
-    });
-  });
+      var animalGIFs = res.data;
 
+      animalGIFs.forEach(element => {
+        animalImg = $('<img>').attr({
+          src: element.images.fixed_width.url,
+          alt: element.title 
+        });
+
+        $('#giffy-ness').append(animalImg);
+      });
+    });
+
+    $('#animal').val('');
+  });
 });
