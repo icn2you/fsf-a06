76224f4a5af6971ccfaaa51ee3b25e8069de04f6
Christopher Zenner <christopher.zenner@gmail.com>
2020-03-21 21:23:31 -0700

    feat: Add GIPHY API call
    - Create and issue query and log result to console (`giffy.js).
    - Change jQuery script to `min`, rather than `slim` for AJAX func-
      tionality.
    - Change form button type to `button`, rather than `submit` to prevent
      page reload.

---
+++ assets/javascript/giffy.js
@@ -0,0 +1,28 @@
+/******************************************************************************
+FSWD:  Christopher B. Zenner
+Date:  03/21/2020
+File:  giffy.js
+Ver.:  0.1.0 20200321
+       
+This JS script is all about API calls and getting a litte "giffy." =P
+******************************************************************************/
+
+$(document).ready(function() {
+
+  $('#get-giffy').on('click', function() {  
+    var animal = $('#animal').val().trim(),
+        queryURL = "https://api.giphy.com/v1/gifs/search?api_key=X6f0Yx1wXCdYoPDqRVxg1BSA17QzInZZ&q=" + animal + "&limit=10&rating";
+
+    // DEBUG:
+    console.log(queryURL);
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    .then (function(res) {
+      console.log(res);
+    });
+  });
+
+});
